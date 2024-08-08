ae2b7c9a1118f89c4bb24efa8d396be34b8869ce
CL.Liu <101190042+PHILL-code-LAB@users.noreply.github.com>
2024-08-08 12:08:03 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -14,7 +14,10 @@ type NotionTask = {
   error?: string;
 };
 
-const { NOTION_TOKEN, NOTION_SPACE_ID, NOTION_USER_ID } = process.env;
+//const { NOTION_TOKEN, NOTION_SPACE_ID, NOTION_USER_ID } = process.env;
+const notionToken = process.env.NOTION_TOKEN;
+const notionSpaceId = process.env.NOTION_SPACE_ID;
+const notionUserId = process.env.NOTION_USER_ID;
 if (!NOTION_TOKEN || !NOTION_SPACE_ID || !NOTION_USER_ID) {
   throw new Error(
     "Environment variable NOTION_TOKEN, NOTION_SPACE_ID or NOTION_USER_ID is missing. Check the README.md for more information."
